ec3936b19cc332755df296f538f1541e9c1e29ca
Hdsenevi <shanaka36@gmail.com>
2020-03-13 09:13:51 +1100

    Initial Commit

---
+++ macos-factors-calculator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  macos-factors-calculator
+//
+//  Created by Shanaka Senevirathne on 13/3/20.
+//  Copyright © 2020 Shanaka Senevirathne. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
